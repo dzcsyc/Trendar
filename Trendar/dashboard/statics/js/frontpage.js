a6245a84a9bd59ff375746cf94f46626df0cb0d6
sylviazmy <anacarro@163.com>
2017-01-12 17:20:57 +0800

    updated frontend

--- Trendar/dashboard/statics/js/frontpage.js
+++ Trendar/dashboard/statics/js/frontpage.js
@@ -1,3 +1,34 @@
 /**
  * Created by I321310 on 12/30/2016.
  */
+function Summary()
+{
+
+var TopReads =[{"content":"So, lucky I wasn’t hand crafting this because I probably would have given up before I typed 7,422 unique RGB combinations (correctly !!).  All I had to do was get my code working properly when writing the files and then interpret that data properly in Tableau."}];
+var content= TopReads[0].content;
+return content;
+
+}
+
+function Time()
+{
+	var UpdateTime=[{"time":"6:25 PM 1/5/2016"}];
+	var time = UpdateTime[0].time;
+	return time;
+}
+//待完成
+function RequestFrontPage() {
+        $.ajax({
+            type: 'GET',
+            url: '',
+            dataType: 'json',
+            contentType: "application/json",
+            success: function (data) {
+
+                return data;
+            },
+            error: function (data) {
+                return 1;
+            }
+        });
+    }
--- Trendar/dashboard/statics/js/sentiment.js
+++ Trendar/dashboard/statics/js/sentiment.js
@@ -1,64 +1,41 @@
-$(function() {
+function display() {
             $.ajax({
                 type: 'GET',
-                url: 'http://127.0.0.1:8000/api/negtotal/',
+                url: 'http://127.0.0.1:8000/api/sentiment/',
                 dataType: 'json',
                 contentType: "application/json",
                 success: function (data) {
-                    var myChart = echarts.init(document.getElementById('main'));
+                    var myChart = echarts.init(document.getElementById('sentimentDistribution'));
                     var a = []
-                    var type = []
                     $.each(data, function(index, item){
                         a.push(item.number)
-                        type.push(item.typeof)
                     });
                     // 指定图表的配置项和数据
                     var option = {
                         title: {
-                            text: '客户抱怨点'
+                            text: 'Trendar 商品评论情感分析'
                         },
                         tooltip: {},
-                        legend: {
-                            data:['评论数']
-                        },
+//                        legend: {
+//                            data:['评论数']
+//                        },
                         xAxis: {
-                            data: type
+                            data: ["0-0.1","0.1-0.2","0.2-0.3","0.3-0.4","0.4-0.5","0.5-0.6","0.6-0.7","0.7-0.8","0.8-0.9","0.9-1"]
                         },
                         yAxis: {},
                         series: [{
-                            name: '差评数',
+                            name: '评论数',
                             type: 'bar',
                             data: a
                         }],
                         color:['#F1B718']
                     };
+
                     // 使用刚指定的配置项和数据显示图表。
                     myChart.setOption(option);
-                    myChart.on('click', function (params) {
-                        $.ajax({
-                            type: 'GET',
-                            url: 'http://127.0.0.1:8000/api/resource/'+params.name,
-                            dataType: 'json',
-                            contentType: "application/json",
-                            success: function (data) {
-                                $("#show").empty()
-                                $.each(data, function(index, item) {
-                                    $("#show").append(
-                                            "<tr>"
-                                            + "<td>" + item.content + "</td>"
-                                            + "</tr>"
-                                    );
-                                })
-                            },
-                            error: function (data) {
-                                alert("error");
-                            }
-                        });
-                    });
                 },
                 error: function (data) {
                     alert("error");
                 }
             });
-        })
-    </script>
+        }
